68282558f7fbb76a3d52818aa41fd7bd4bcf3cd3
yeeway <yiwei.suuu@gmail.com>
2025-02-19 23:57:33 +0800

    feat: add debug component with performance controls

--- src/main.jsx
+++ src/main.jsx
@@ -1,37 +1,44 @@
-import ReactDOM from "react-dom/client";
-import { Canvas } from "@react-three/fiber";
-import * as THREE from "three";
-import "./style.css";
-import App from "./component/55-App";
-import Drei from "./component/56-Drei";
-import MouseEvent from "./component/62-MouseEvent";
+import ReactDOM from "react-dom/client"
+import { Canvas } from "@react-three/fiber"
+import * as THREE from "three"
+import "./style.css"
+import App from "./component/55-App"
+import Drei from "./component/56-Drei"
+import MouseEvent from "./component/62-MouseEvent"
 
-const root = ReactDOM.createRoot(document.querySelector("#root"));
+import Debug from "./component/57-Debug"
+import { StrictMode } from "react"
+import { Leva } from "leva"
+
+const root = ReactDOM.createRoot(document.querySelector("#root"))
 
 root.render(
-  <Canvas
-    // orthographic
-    camera={{
-      // zoom: 50,
-      position: [3, 2, 6],
-      // fov: 95,
-      // near: 0.1,
-      // far: 200,
-    }}
-    dpr={[1, 2, 3]}
-    gl={{
-      // antialias: false,
-      toneMapping: THREE.CineonToneMapping,
-      outputColorSpace: THREE.SRGBColorSpace,
-    }}
-  >
-    {/* 55. First R3F Application */}
-    {/* <App /> */}
+  <StrictMode>
+    <Leva collapsed />
 
-    {/* 56. Drei */}
-    {/* <Drei /> */}
+    <Canvas
+      // orthographic
+      camera={{
+        // zoom: 50,
+        position: [3, 2, 6],
+        // fov: 95,
+        // near: 0.1,
+        // far: 200,
+      }}
+      dpr={[1, 2, 3]}
+      gl={{
+        // antialias: false,
+        toneMapping: THREE.CineonToneMapping,
+        outputColorSpace: THREE.SRGBColorSpace,
+      }}
+    >
+      {/* <App /> */}
 
-    {/* 62. Mouse Event */}
-    <MouseEvent />
-  </Canvas>
-);
+      {/* <Drei /> */}
+
+      {/* <MouseEvent /> */}
+
+      <Debug />
+    </Canvas>
+  </StrictMode>
+)
